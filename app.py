d490f7727d96cd3cf59855e77da9ea44debdcc5c
Subalakshmi-80 <subaproj3440@gmail.com>
2026-02-07 12:02:27 +0530

    Adding app changes app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello():
-    name = request.args.get("name", "World")
+@app.route("/batman")
+def batman():
+    name = request.args.get("name", "Bruce Wayne")
     return jsonify({
-        "message": f"Hello {name}"
+        "message": f"Hello {}"
     })
 
 if __name__ == "__main__":
