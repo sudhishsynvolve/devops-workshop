2eee2e367c47275df1acdc3f3d1882e1d21d2af2
madhu004 <26madhu20042gmail.com>
2026-02-07 12:15:42 +0530

    updated

---
+++ Thanos.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    name = request.args.get("name", "World")
+    return jsonify({
+        "message": f"Hello {name}"
+    })
+
+@app.route("/thanos")
+def thanos():
+    name = request.args.get("name", "thanos")
+    return jsonify({
+        "message": f"Hello {name}"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
