0801ea8890ab1cfe19fff6915d6b7729e117e747
kanis-2129 <kaniskasinatha@gmail.com>
2026-02-07 12:24:09 +0530

    Adding addition .py and textfile

--- app.py
+++ app.py
@@ -9,5 +9,12 @@ def hello():
         "message": f"Hello {name}"
     })
 
+@app.route("/flash")
+def flash():
+    name = request.args.get("name", "Flash")
+    return jsonify({
+        "message": f"Hello {name}"
+    })
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
