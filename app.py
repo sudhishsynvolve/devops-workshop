d2b64299887cc913c3ecce138d39457177ab4c7b
jaya <jvarshiniegs@gmail.com>
2026-02-07 11:57:15 +0530

    initial commit

--- app.py
+++ app.py
@@ -9,5 +9,13 @@ def hello():
         "message": f"Hello {name}"
     })
 
+@app.route("/blackwidow")
+def blackwidow():
+    name = request.args.get("name", "This is Black Widow")
+    return jsonify({
+        "message": f"Hello {name}"
+    })
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
