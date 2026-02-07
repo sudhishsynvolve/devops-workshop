83e0d770ba80291e93dba05cc7057fb0f09a51e7
Boojashrigunasekaran <you@example.com>
2026-02-07 12:12:39 +0530

    Addding app.py

--- app.py
+++ app.py
@@ -9,5 +9,12 @@ def hello():
         "message": f"Hello {name}"
     })
 
+@app.route("/spiderman")
+def spiderman():
+    name = request.args.get("name", "mary jane")
+    return jsonify({
+        "message": f"Hello {name}"
+    })
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
