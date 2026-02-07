3dcbc3b8e7db23c1c8517b51d41b034baac86f85
Subalakshmi-80 <subaproj3440@gmail.com>
2026-02-07 12:21:39 +0530

    adding app changes

--- app.py
+++ app.py
@@ -2,12 +2,27 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/batman")
-def batman():
-    name = request.args.get("name", "Bruce Wayne")
+@app.route("/hello")
+def hello():
+    name = request.args.get("name", "world")
     return jsonify({
-        "message": f"Hello {}"
+        "message": f"Hello {name}"
     })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/batman")
+def batman():
+    name = request.args.get("name", "Bruce Wayne")
+    return jsonify({
+        "message": f"Hello {name}"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
